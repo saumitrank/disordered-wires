2fea1f82c29aae3a01be21ea4965619e8eefda03
saumitrank <saumitrank@gmail.com>
2022-09-07 18:50:54 -0500

    adding kane-mele ribbon function and create utils

--- src/systems.py
+++ src/systems.py
@@ -201,3 +201,111 @@ def armchair(params:dict)->kwant.system.FiniteSystem:
             sys.attach_lead(lead_bot)
     
     return sys.finalized()
+
+
+
+
+
+def kane_mele(params:dict)->kwant.system.FiniteSystem:
+    """Create a zigzag Kane-Mele ribbon with NN and SOC
+
+    Args:
+        params (dict): A dictionary of parameters for the Kane-Mele ribbon. 
+        This contains:
+        a (float): Lattice constant 
+        t (float): NN hopping
+        t2 (float): NNN SOC
+        mu (float): Chemical potential in scattering region
+        mul (float): Additional potential in leads
+        L (int): Length
+        W (int): Number of chains
+        Nimp (float): Probability of bond/site having disorder (from 0 to 1)
+        V (float): Strength of disorder chosen from uniform distribution in [-V, V]
+        dis (0, 1): Disorder type. 0 for on-site, 1 for hopping
+        leads (bool): Whether or not there should be leads
+
+    Returns:
+        kwant.system.FiniteSystem: Arm-chair ribbon with / without leads attached
+    """
+    
+    #Default values
+    p = {'a' : 1.0, 't' : 1.0, 't2' : 0.3, 't3' : 0.3, 'mu' : 0.0, 'mul' : 0.0, 
+         'L' : 10, 'W' : 10, 'Nimp' : 1.0, 'V' : 0.0, 'dis' : 0,
+         'leads':True}
+    
+    #Update params
+    p.update(params)  
+    
+    #Define Pauli matrices
+    sigma = np.array((((1, 0), (0, 1)),
+                      ((0, 1), (1, 0)),
+                      ((0, -1j), (1j, 0)),
+                      ((1, 0), (0, -1))))
+    
+    #Define lattice and sub-lattices with spin
+    lat = kwant.lattice.honeycomb(p['a'], norbs=2)
+    B, A = lat.sublattices
+    sys = kwant.Builder()
+    
+    #Create sites and NN hopping
+    #Special case: 1D chain
+    if p['W']==1:
+        for i in range(p['L']):
+            sys[A(i, 0)] = p['mu']*sigma[0]
+            sys[B(i, 1)] = p['mu']*sigma[0]
+
+        sys[lat.neighbors()] = p['t']*sigma[0]
+
+    #For W>1
+    else:
+        for i in range(p['L']):
+            for j in range(p['W']+1):
+                sys[A(i-j//2, j)] = p['mu']*sigma[0]
+                sys[B(i-j//2, j)] = p['mu']*sigma[0]
+                if j%2 == 0:
+                    sys[A(p['L']-j//2, j)] = p['mu']*sigma[0]
+                    sys[B(p['L']-j//2, j)] = p['mu']*sigma[0]
+        
+        sys[lat.neighbors()] = p['t']*sigma[0]
+        sys.eradicate_dangling()    
+    
+    #Add disorder
+    sys = add_disorder(sys, lat, p['Nimp'], p['V'], p['dis'])
+    
+    #NNN spin orbit term
+    kinds = [(1, 0), (0, 1), (1, -1)]
+    
+    nuA = [1, 0, -1]  #Spin orbit sign for A to A
+    
+    for kind, nu in zip(kinds, nuA):
+        sys[kwant.builder.HoppingKind(kind, A)] =  1j*p['t2']*nu*sigma[3]
+        sys[kwant.builder.HoppingKind(kind, B)] = -1j*p['t2']*nu*sigma[3]
+      
+    #Add Leads
+    if (p['leads']):
+        #Create left leads
+        sym_left = kwant.TranslationalSymmetry(lat.vec((-1, 0)))
+        lead_left = kwant.Builder(sym_left)
+        
+        for j in range(p['W']+1):
+                lead_left[A(0, j)] = (p['mu'] + p['mul'])*sigma[0]
+                lead_left[B(0, j)] = (p['mu'] + p['mul'])*sigma[0]
+        
+        lead_left[lat.neighbors()] = p['t']*sigma[0]
+        lead_left.eradicate_dangling()
+
+        #NNN spin orbit term
+        kinds = [(1, 0), (0, 1), (1, -1)]
+        nuA = [1, 0, -1]  #Spin orbit sign for A to A
+        
+        for kind, nu in zip(kinds, nuA):
+            lead_left[kwant.builder.HoppingKind(kind, A)] =  1j*p['t2']*nu*sigma[3]
+            lead_left[kwant.builder.HoppingKind(kind, B)] = -1j*p['t2']*nu*sigma[3]
+        
+        lead_right = lead_left.reversed()
+        
+        #Attach leads
+        sys.attach_lead(lead_left)
+        sys.attach_lead(lead_right)
+
+    return sys.finalized()
